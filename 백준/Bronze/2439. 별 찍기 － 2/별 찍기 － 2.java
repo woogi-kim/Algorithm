289725e5b3dfac345b7e8959561ef339250b911d
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2023-06-19 17:08:51 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            int n = Integer.parseInt(bufferedReader.readLine());
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (j < n - i - 1) {
+                        bufferedWriter.write(" ");
+                    } else {
+                        bufferedWriter.write("*");
+                    }
+                }
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
