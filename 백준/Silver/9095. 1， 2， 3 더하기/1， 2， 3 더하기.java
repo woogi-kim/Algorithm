58fede5d7c092724b44aab19c146530c6a726357
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2023-06-21 15:33:40 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static int[] dp;
+
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int n = Integer.parseInt(bufferedReader.readLine());
+            int number;
+            int maxNum = 4;
+            dp = new int[12];
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+            for (int i = 0; i < n; i++) {
+                number = Integer.parseInt(bufferedReader.readLine());
+                if (dp[number] != 0) {
+                    System.out.println(dp[number]);
+                    continue;
+                }
+
+                for (int j = maxNum; j <= number; j++) {
+                    dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+                }
+                maxNum = number;
+                System.out.println(dp[number]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
