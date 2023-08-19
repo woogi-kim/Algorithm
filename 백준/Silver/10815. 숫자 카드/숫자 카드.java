ebb92808e16847bea9e8ba24eb60d1cf619b2762
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2023-08-20 02:13:46 +0900

    [Silver V] Title: 숫자 카드, Time: 1320 ms, Memory: 108420 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int n, m;
+    public static int[] own;
+    public static int[] check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        n = Integer.parseInt(bufferedReader.readLine());
+        own = new int[n];
+        st = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+            own[i] = Integer.parseInt(st.nextToken());
+        }
+
+        m = Integer.parseInt(bufferedReader.readLine());
+        check = new int[m];
+        st = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < m; i++) {
+            check[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(own);
+
+        for (int i = 0; i < m; i++) {
+            sb.append(binarySearch(i)).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    public static int binarySearch(int i) {
+        int end = n - 1;
+        int start = 0;
+        int mid;
+        while (end >= start) {
+            mid = (end + start) / 2;
+            if (check[i] == own[mid]) {
+                return 1;
+            } else if (check[i] > own[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return 0;
+    }
+}
