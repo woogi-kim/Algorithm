02809186c80dd43ca11e2bcf5a0e0499a5d11d9a
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2023-09-14 19:13:46 +0900

    [Silver IV] Title: ATM, Time: 152 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static Integer arr[];
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new Integer[n];
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return o1 - o2;
+                    }
+                }
+        );
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int tmp = 0;
+            for (int j = 0; j <= i; j++) {
+                tmp += arr[j];
+            }
+            sum += tmp;
+        }
+        System.out.println(sum);
+    }
+}
