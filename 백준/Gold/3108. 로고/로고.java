07223e293bddce096592f2f28015d17f0324b90a
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2023-11-07 16:21:05 +0900

    [Gold II] Title: 로고, Time: 228 ms, Memory: 27276 KB -BaekjoonHub

--- 백준/Gold/3108. 로고/로고.java
+++ 백준/Gold/3108. 로고/로고.java
@@ -3,79 +3,63 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Rec {
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-
-    public Rec(int x1, int y1, int x2, int y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-    }
-}
-
 public class Main {
     public static int n;
     public static int ans;
-    public static boolean[] visited;
-    public static Rec[] rec;
+    public static boolean[][] visited = new boolean[2001][2001];
+    public static boolean[][] isRectangle = new boolean[2001][2001];
     public static int x1, y1, x2, y2;
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
-        visited = new boolean[n + 1];
-        rec = new Rec[n + 1];
 
-        rec[0] = new Rec(0, 0, 0, 0);
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            x1 = Integer.parseInt(st.nextToken());
-            y1 = Integer.parseInt(st.nextToken());
-            x2 = Integer.parseInt(st.nextToken());
-            y2 = Integer.parseInt(st.nextToken());
-            rec[i] = new Rec(x1, y1, x2, y2);
-        }
-
-        for (int i = 0; i <= n; i++) {
-            if (!visited[i]) {
-                bfs(i);
-                ans++;
+            x1 = (Integer.parseInt(st.nextToken()) + 500) * 2;
+            y1 = (Integer.parseInt(st.nextToken()) + 500) * 2;
+            x2 = (Integer.parseInt(st.nextToken()) + 500) * 2;
+            y2 = (Integer.parseInt(st.nextToken()) + 500) * 2;
+            for (int j = x1; j <= x2; j++) {
+                isRectangle[j][y1] = true;
+                isRectangle[j][y2] = true;
             }
+            for (int j = y1; j <= y2; j++) {
+                isRectangle[x1][j] = true;
+                isRectangle[x2][j] = true;
+            }
+
         }
 
-        System.out.println(ans-1);
-    }
-
-    public static void bfs(int k) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(k);
-        visited[k] = true;
-        while (!q.isEmpty()) {
-            Integer current = q.remove();
-            for (int i = 0; i <= n; i++) {
-                if (current == i || !isContained(current, i) || visited[i]) {
-                    continue;
+        for (int i = 0; i <= 2000; i++) {
+            for (int j = 0; j <= 2000; j++) {
+                if (!visited[i][j] && isRectangle[i][j]) {
+                    dfs(i, j);
+                    ans++;
                 }
-                q.add(i);
-                visited[i] = true;
-
             }
         }
+        if (isRectangle[1000][1000]) {
+            ans--;
+        }
+
+        System.out.println(ans);
     }
 
-    public static boolean isContained(int current, int next) {
-        Rec c = rec[current];
-        Rec n = rec[next];
-        if ((c.x1 < n.x1 && n.x2 < c.x2 && c.y1 < n.y1 && n.y2 < c.y2)
-                || (c.x1 > n.x1 && n.x2 > c.x2 && c.y1 > n.y1 && n.y2 > c.y2)
-                || c.x2 < n.x1 || c.x1 > n.x2 || c.y2 < n.y1 || c.y1 > n.y2) {
-            return false;
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if (nextX >= 0 && nextX <= 2000 && nextY >= 0 && nextY <= 2000) {
+                if (!visited[nextX][nextY] && isRectangle[nextX][nextY]) {
+                    dfs(nextX, nextY);
+                }
+            }
+
         }
-        return true;
     }
 
 }
