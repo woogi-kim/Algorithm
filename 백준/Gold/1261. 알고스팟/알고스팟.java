aa36e8d42917d9ad6824f17da042351f4d1ac7c9
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2023-11-14 16:36:30 +0900

    [Gold IV] Title: 알고스팟, Time: 140 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
-class Node {
+class Node implements Comparable<Node> {
     int x, y;
+    int minBreak;
 
-    public Node(int x, int y) {
+    public Node(int x, int y, int minBreak) {
         this.x = x;
         this.y = y;
+        this.minBreak = minBreak;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.minBreak - o.minBreak;
     }
 }
 
@@ -18,7 +23,6 @@ public class Main {
     public static int ans = Integer.MAX_VALUE;
     public static int n, m;
     public static char[][] input;
-    public static int[][] minBreak;
     public static boolean[][] visited;
     public static int[] dx = {1, 0, -1, 0};
     public static int[] dy = {0, 1, 0, -1};
@@ -30,52 +34,45 @@ public class Main {
         m = Integer.parseInt(tmp[0]);
         n = Integer.parseInt(tmp[1]);
         input = new char[n][m];
-        minBreak = new int[n][m];
         visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             input[i] = bf.readLine().toCharArray();
-            Arrays.fill(minBreak[i], Integer.MAX_VALUE);
         }
-        minBreak[0][0] = 0;
         bfs();
-        System.out.println(minBreak[n - 1][m - 1]);
+        System.out.println(ans);
 
     }
 
     public static void bfs() {
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0, 0));
+        Queue<Node> q = new PriorityQueue<>();
+        q.add(new Node(0, 0, 0));
+        visited[0][0] = true;
         while (!q.isEmpty()) {
             Node current = q.remove();
             if (current.x == n - 1 && current.y == m - 1) {
-                continue;
+                ans = current.minBreak;
+                break;
             }
             for (int i = 0; i < 4; i++) {
                 int newX = current.x + dx[i];
                 int newY = current.y + dy[i];
 
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
-                    if (input[newX][newY] == '1') {
-                        if (minBreak[current.x][current.y] + 1 < minBreak[newX][newY]) {
-                            q.add(new Node(newX, newY));
-                            minBreak[newX][newY] = minBreak[current.x][current.y] + 1;
-                            visited[newX][newY] = true;
-                        }
-                    } else {
-                        if (!visited[newX][newY]) {
-                            q.add(new Node(newX, newY));
-                            minBreak[newX][newY] = minBreak[current.x][current.y];
-                            visited[newX][newY] = true;
-                        } else {
-                            if (minBreak[current.x][current.y] < minBreak[newX][newY]) {
-                                q.add(new Node(newX, newY));
-                                minBreak[newX][newY] = minBreak[current.x][current.y];
-                            }
-                        }
-
-                    }
+                if (!(newX >= 0 && newX < n && newY >= 0 && newY < m)) {
+                    continue;
                 }
+                if (visited[newX][newY]) {
+                    continue;
+                }
+
+                int nextBreakNum = current.minBreak;
+                if (input[newX][newY] == '1') {
+                    nextBreakNum++;
+                }
+
+                q.add(new Node(newX, newY, nextBreakNum));
+                visited[newX][newY] = true;
             }
         }
     }
 }
+
