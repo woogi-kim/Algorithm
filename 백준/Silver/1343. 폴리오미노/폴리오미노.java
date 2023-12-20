4edd0849624754af55276dc710940b0f6ec1a941
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2023-12-20 19:23:50 +0900

    [Silver V] Title: 폴리오미노, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -1,49 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String input = bf.readLine();
-        int countX = 0;
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == 'X') {
-                countX++;
-            } else {
-                if (countX % 2 == 1) {
-                    System.out.println(-1);
-                    return;
-                } else {
-                    while (countX >= 4) {
-                        countX -= 4;
-                        sb.append("AAAA");
-                    }
-                    if (countX == 2) {
-                        countX -= 2;
-                        sb.append("BB");
-                    }
-                    sb.append('.');
-                }
-            }
-        }
-        if (countX % 2 == 1) {
+
+        input = input.replaceAll("XXXX", "AAAA");
+        input = input.replaceAll("XX", "BB");
+        if (input.contains("X")) {
             System.out.println(-1);
-            return;
         } else {
-            while (countX >= 4) {
-                countX -= 4;
-                sb.append("AAAA");
-            }
-            if (countX == 2) {
-                countX -= 2;
-                sb.append("BB");
-            }
+            System.out.println(input);
         }
-        System.out.println(sb);
     }
 
 }
