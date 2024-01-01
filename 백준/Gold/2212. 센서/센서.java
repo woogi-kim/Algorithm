b3ea7c4173c38c7179cf1daa0e2f6f68348a9593
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2024-01-01 18:21:41 +0900

    [Gold V] Title: 센서, Time: 192 ms, Memory: 16284 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static long ans;
+    public static int n;
+    public static int k;
+    public static int[] input;
+    public static int[] diff;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        k = Integer.parseInt(bf.readLine());
+        input = new int[n];
+        diff = new int[n - 1];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+        for (int i = 0; i < n - 1; i++) {
+            diff[i] = input[i + 1] - input[i];
+        }
+        Arrays.sort(diff);
+        for (int i = 0; i < n - k; i++) {
+            ans += diff[i];
+        }
+        System.out.println(ans);
+    }
+
+
+}
+
+
