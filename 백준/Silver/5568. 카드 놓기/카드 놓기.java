71080443b7e815c02ce20a6f413e1b44f40b715c
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2024-01-07 20:37:59 +0900

    [Silver IV] Title: 카드 놓기, Time: 744 ms, Memory: 297240 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int k;
+    public static int[] input;
+    public static boolean[] isIncluded;
+    public static ArrayList<String> arr = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        k = Integer.parseInt(bf.readLine());
+        input = new int[n];
+        isIncluded = new boolean[n];
+        Arrays.fill(isIncluded, false);
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(bf.readLine());
+        }
+        solve(0, "");
+        System.out.println(arr.size());
+    }
+
+    public static void solve(int depth, String current) {
+        if (depth == k && !arr.contains(current)) {
+            arr.add(current);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (isIncluded[i]) {
+                continue;
+            }
+            isIncluded[i] = true;
+            solve(depth + 1, current + input[i]);
+            isIncluded[i] = false;
+        }
+    }
+
+
+}
+
+
