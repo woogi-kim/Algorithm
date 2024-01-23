669a944ea44501a3a108b0e7934283ca5b53f3af
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2024-01-23 15:37:01 +0900

    [Gold IV] Title: 줄세우기, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int h;
+    public static int[] arr;
+    public static int[] dp;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        arr = new int[n];
+        dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(n - max);
+    }
+
+
+}
