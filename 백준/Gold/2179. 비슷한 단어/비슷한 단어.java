81be0a7057c2124aca86e351938e82a0e5965380
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2025-08-21 15:52:56 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1412 ms, Memory: 18100 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static String[] words;
+	public static int n;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(bf.readLine());
+
+		words = new String[n];
+		for (int i = 0; i < n; i++) {
+			words[i] = bf.readLine();
+		}
+
+		int max = 0;
+		String first = "";
+		String second = "";
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				int duplicateLength = getDuplicateLength(words[i], words[j]);
+
+				if (duplicateLength > max) {
+					max = duplicateLength;
+					first = words[i];
+					second = words[j];
+				}
+			}
+		}
+
+		System.out.println(first);
+		System.out.println(second);
+
+	}
+
+	public static int getDuplicateLength(String a, String b) {
+		int len = Math.min(a.length(), b.length());
+
+		int count = 0;
+		for (int i = 0; i < len; i++) {
+			if (a.charAt(i) == b.charAt(i)) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		return count;
+	}
+}
