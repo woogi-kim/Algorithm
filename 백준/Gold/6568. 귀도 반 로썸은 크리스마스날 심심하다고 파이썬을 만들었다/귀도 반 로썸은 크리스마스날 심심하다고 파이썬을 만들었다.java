9d65db17fab9120b3e74645c43797a97bb93db78
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2025-08-25 23:35:28 +0900

    [Gold V] Title: 귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

--- 백준/Gold/6568. 귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다/귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다.java
+++ 백준/Gold/6568. 귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다/귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다.java
@@ -1,76 +1,105 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
 public class Main {
-    public static String[] commands;
-    public static int adder;
-    public static int pc;
+	public static String[] commands;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            commands = new String[32];
-            for (int i = 0; i < 32; i++) {
-                String line = bf.readLine();
-                if (line == null) return;          // EOF
-                commands[i] = line;
-            }
+	public static int adder;
+	public static int pc;
 
-            // 테스트 케이스마다 초기화
-            adder = 0;
-            pc = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			commands = new String[32];
+			for (int i = 0; i < 32; i++) {
+				if ((commands[i] = bf.readLine()) == null) {
+					return;
+				}
+			}
+			pc = 0;
+			adder = 0;
 
-            while (true) {
-                String curCommand = commands[pc];
-                pc = (pc + 1) & 31;               // 5비트 PC: 자동 증가 후 모듈로 32
+			while (true) {
+				String curCommand = commands[pc];
+				// System.out.println("현재 명령어 " + commands[pc]);
+				pc = (pc + 1) % 32;
 
-                String commandType = curCommand.substring(0, 3);
-                int targetAddress = convertBinaryToInt(curCommand.substring(3)); // 0~31
+				String commandType = curCommand.substring(0, 3);
 
-                if (commandType.equals("000")) {          // STA x
-                    commands[targetAddress] = convertIntToBinary(adder);
-                } else if (commandType.equals("001")) {   // LDA x
-                    adder = convertBinaryToInt(commands[targetAddress]);
-                } else if (commandType.equals("010")) {   // BEQ x
-                    if (adder == 0) pc = targetAddress;   // x는 이미 0~31
-                } else if (commandType.equals("011")) {   // NOP
-                    // do nothing
-                } else if (commandType.equals("100")) {   // DEC
-                    adder = wrap8(adder - 1);
-                } else if (commandType.equals("101")) {   // INC
-                    adder = wrap8(adder + 1);
-                } else if (commandType.equals("110")) {   // JMP x
-                    pc = targetAddress;
-                } else {                                  // 111 ----- HLT
-                    break;
-                }
-            }
-            System.out.println(convertIntToBinary(adder));
-        }
-    }
+				int targetAddress = convertBinaryToInt(curCommand.substring(3));
+				// System.out.println("타켓 주소 " + convertBinaryToInt(curCommand.substring(3)));
 
-    // 8비트 2진수 문자열 -> int(0~255)
-    public static int convertBinaryToInt(String s) {
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            res = (res << 1) | (s.charAt(i) - '0');
-        }
-        return res & 0xFF;
-    }
+				if (commandType.equals("000")) {
+					commands[targetAddress] = convertIntToBinary(adder);
+				} else if (commandType.equals("001")) {
+					adder = convertBinaryToInt(commands[targetAddress]);
+				} else if (commandType.equals("010")) {
+					if (adder == 0) {
+						pc = targetAddress;
+					}
+				} else if (commandType.equals("011")) {
+					continue;
+				} else if (commandType.equals("100")) {
+					adder = (adder - 1) & 0xFF;
+				} else if (commandType.equals("101")) {
+					adder = (adder + 1) & 0xFF;
+				} else if (commandType.equals("110")) {
+					pc = targetAddress;
+				} else {
+					break;
+				}
+				// System.out.println("가산기 값 : " + adder);
+			}
+			System.out.println(convertIntToBinary(adder));
 
-    // int 어떤 값이든 8비트로 래핑 후 8비트 2진수 문자열
-    public static String convertIntToBinary(int n) {
-        int v = wrap8(n);
-        String b = Integer.toBinaryString(v);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8 - b.length(); i++) sb.append('0');
-        sb.append(b);
-        return sb.toString();
-    }
+		}
 
-    // 0~255로 모듈러
-    private static int wrap8(int x) {
-        return ((x % 256) + 256) % 256;
-    }
+	}
+
+	public static int convertBinaryToInt(String s) {
+		// System.out.println(s);
+		int res = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '1') {
+				res += (int)Math.pow(2, s.length() - i - 1);
+				// System.out.println("i: " + i);
+				// System.out.println(res);
+			}
+		}
+
+		return res;
+	}
+
+	public static String convertIntToBinary(int n) {
+		StringBuilder sb = new StringBuilder();
+		while (n > 0) {
+			sb.append(n % 2);
+
+			n /= 2;
+		}
+
+		sb = sb.reverse();
+
+		if (sb.length() > 8) {
+			return sb.substring(sb.length() - 8);
+		} else {
+			sb = sb.reverse();
+			while (sb.length() < 8) {
+				sb.append(0);
+			}
+
+			return sb.reverse().toString();
+		}
+	}
+
 }
