a3435edbacaf9bb107b9b5e1dadb1c998b0fc35d
Kim Min Uk <62949434+woogi-kim@users.noreply.github.com>
2025-09-25 00:39:39 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,7 +1,20 @@
--- 코드를 작성해주세요
+# select i.id as id, n.FISH_NAME as fish_name, max(i.length) as length
+# from FISH_INFO as i
+# left join FISH_NAME_INFO as n on i.fish_type = n.fish_type 
+# where i.length is not null
+# group by n.fish_name
+# order by i.id;
 
-select i.id, n.fish_name, i.length
-from FISH_INFO as i
-left join fish_name_info as n on i.fish_type = n.fish_type
-where (i.fish_type, i.LENGTH) in (select ii.fish_type, max(coalesce(ii.LENGTH, 0)) as length from fish_info as ii group by ii.fish_type)
-order by i.id asc
+select fish_info.id as ID, fish_name_info.fish_name as fish_name, maxlen.length as length
+from (select fish_type, max(length) as length
+        from fish_info
+        where length is not null
+        group by fish_type) as maxlen
+join fish_info on maxlen.fish_type = fish_info.fish_type and maxlen.length = fish_info.length
+join fish_name_info on fish_name_info.fish_type = maxlen.fish_type
+order by fish_info.id asc
+
+# select fish_type, max(length) as length
+#         from fish_info
+#         where length is not null
+#         group by fish_type
